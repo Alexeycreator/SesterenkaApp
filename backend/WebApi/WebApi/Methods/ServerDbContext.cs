769f484d5b2e299c73cb884abcd2afaefff9bba1
Alexeycreator <rushev.alexey@gmail.com>
2026-02-28 13:23:00 +0300

    Добавлены новые сущности

--- backend/WebApi/WebApi/Methods/ServerDbContext.cs
+++ backend/WebApi/WebApi/Methods/ServerDbContext.cs
@@ -3,20 +3,26 @@ using WebApi.Models.DataBase;
 
 namespace WebApi.Methods;
 
-public sealed class ServerDbContext : DbContext
+public sealed class ServerDbContext(DbContextOptions<ServerDbContext> options) : DbContext(options)
 {
-    public DbSet<ClientModel> Clients { get; set; }
-    public DbSet<AddressModel> Addresses { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseSqlServer(
-            $@"Server=(local)\SQLEXPRESS;Database=ShesterenkaApp;Trusted_Connection=True;TrustServerCertificate=True;");
-    }
+    public DbSet<ClientsModel> Clients { get; set; }
+    public DbSet<AddressesModel> Addresses { get; set; }
+    public DbSet<CarModelsModel> CarModels { get; set; }
+    public DbSet<CarBrandsModel> CarBrands { get; set; }
+    public DbSet<CarModificationsModel> CarModifications { get; set; }
+    public DbSet<CategoriesModel> Categories { get; set; }
+    public DbSet<EmployeesModel> Employees { get; set; }
+    public DbSet<ManufacturersModel> Manufacturers { get; set; }
+    public DbSet<OrdersModel> Orders { get; set; }
+    public DbSet<OrderItemsModel> OrderItems { get; set; }
+    public DbSet<ProductCarApplicabilityModel> ProductCarApplicability { get; set; }
+    public DbSet<ProductsModel> Products { get; set; }
+    public DbSet<StocksModel> Stocks { get; set; }
+    public DbSet<WarehousesModel> Warehouses { get; set; }
+    public DbSet<WarehousesAddressesModel> WarehousesAddresses { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
-        builder.Entity<ClientModel>().HasIndex(c => c.Login).IsUnique();
-        builder.Entity<ClientModel>().HasIndex(c => c.NumberPhone).IsUnique();
+        base.OnModelCreating(builder);
     }
 }
