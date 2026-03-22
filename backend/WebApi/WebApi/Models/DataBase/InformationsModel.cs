d3bdb11db8f40e70dd32a8f58772886d14f09ab7
Alexeycreator <rushev.alexey@gmail.com>
2026-03-22 13:38:31 +0300

    Добавлена новая сущность БД

---
+++ backend/WebApi/WebApi/Models/DataBase/InformationsModel.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Models.DataBase;
+
+[Table("Informations")]
+public sealed class InformationsModel
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; private set; }
+
+    [MaxLength(2000)] public string? AboutUs { get; set; }
+    [MaxLength(1000)] public string? Questions { get; set; }
+
+    [Column("Users_Id")]
+    [ForeignKey("Users")]
+    public int? Users_Id { get; set; }
+
+    [Column("Addresses_Id")]
+    [ForeignKey("Addresses")]
+    public int? Addresses_Id { get; set; }
+
+    [JsonIgnore]
+    [DeleteBehavior(DeleteBehavior.Cascade)]
+    public UsersModel? Users { get; set; }
+
+    [JsonIgnore]
+    [DeleteBehavior(DeleteBehavior.Cascade)]
+    public AddressesModel? Addresses { get; set; }
+}
