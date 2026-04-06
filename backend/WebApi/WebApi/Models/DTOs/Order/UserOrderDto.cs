b1b36a7f13679f0058df008b661480027d0430ca
Alexeycreator <rushev.alexey@gmail.com>
2026-04-06 20:34:58 +0300

    Добавлена модель DTO: для пользователя при оформлении заказа

--- backend/WebApi/WebApi/Models/DTOs/Order/UserOrderDto.cs
+++ backend/WebApi/WebApi/Models/DTOs/Order/UserOrderDto.cs
@@ -1,6 +1,11 @@
 namespace WebApi.Models.DTOs.Order;
 
-public class UserOrderDto
+public sealed class UserOrderDto
 {
-    
+    public string? Login { get; set; }
+    public string SecondName { get; set; }
+    public string FirstName { get; set; }
+    public string? SurName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
 }
