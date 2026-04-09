b5ed350a6a2abfbaff033b1abfc32d164126f86a
Alexeycreator <rushev.alexey@gmail.com>
2026-04-09 08:53:27 +0300

    Обновлена модель DTO: исправлена ошибка со списанием товара

--- backend/WebApi/WebApi/Models/DTOs/OrderItem/OrderItemDataDto.cs
+++ backend/WebApi/WebApi/Models/DTOs/OrderItem/OrderItemDataDto.cs
@@ -3,6 +3,7 @@ namespace WebApi.Models.DTOs.OrderItem;
 public sealed class OrderItemDataDto
 {
     public int Id { get; set; }
+    public int ProductId { get; set; }
     public int Quantity { get; set; }
     public double PriceAtMoment { get; set; }
     public string NameCategories { get; set; } = string.Empty;
