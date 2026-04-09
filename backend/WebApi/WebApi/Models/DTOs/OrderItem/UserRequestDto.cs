b3e109727f62f04c4df124f9611742bfc45cf82a
Alexeycreator <rushev.alexey@gmail.com>
2026-04-09 09:00:53 +0300

    Обновлена модель DTO: убраны лишние поля

--- backend/WebApi/WebApi/Models/DTOs/OrderItem/UserRequestDto.cs
+++ backend/WebApi/WebApi/Models/DTOs/OrderItem/UserRequestDto.cs
@@ -2,7 +2,6 @@ namespace WebApi.Models.DTOs.OrderItem;
 
 public class UserRequestDto
 {
-    public int Id { get; set; }
     public string? LoginUser { get; set; }
     public string? RoleUser { get; set; }
 }
