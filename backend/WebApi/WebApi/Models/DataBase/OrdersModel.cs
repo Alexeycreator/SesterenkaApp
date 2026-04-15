3ea0e2ffa558469f5ce24c6262f4b7b2bcc6a079
Alexeycreator <rushev.alexey@gmail.com>
2026-04-15 12:56:09 +0300

    Обновлена модель БД: добавлено имя заказа и внешний ключ на адреса

--- backend/WebApi/WebApi/Models/DataBase/OrdersModel.cs
+++ backend/WebApi/WebApi/Models/DataBase/OrdersModel.cs
@@ -13,16 +13,29 @@ public sealed class OrdersModel
     public int Id { get; private set; }
 
     [Required] public DateTime OrderDate { get; set; } = DateTime.Now;
-    //[Required] [MaxLength(100)] public string NameOrder { get; set; }
-    [Required] [MaxLength(25)] public string Status { get; set; } = "Выдан";
+    [Required] [MaxLength(100)] public string NameOrder { get; set; }
+    [Required] [MaxLength(25)] public string Status { get; set; } = "В корзине";
 
     [Column("Users_Id")]
     [ForeignKey("Users")]
     public int? Users_Id { get; set; }
+
+    [Column("Addresses_Id")]
+    [ForeignKey("Addresses")]
+    public int? Addresses_Id { get; set; }
+
+    [JsonIgnore]
+    [DeleteBehavior(DeleteBehavior.Restrict)]
+    public AddressesModel? Addresses { get; set; }
 
     [JsonIgnore]
     [DeleteBehavior(DeleteBehavior.Restrict)]
     public UsersModel? Users { get; set; }
 
     [JsonIgnore] public ICollection<OrderItemsModel>? OrderItems { get; set; }
+
+    public OrdersModel()
+    {
+        NameOrder = $"Заказ_{OrderDate.ToShortDateString()}_{Id}";
+    }
 }
