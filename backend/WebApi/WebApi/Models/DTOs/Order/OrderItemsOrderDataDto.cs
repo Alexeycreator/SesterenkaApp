0fcb7a238ee5d263e17c71f9d24b951066c62f95
Alexeycreator <rushev.alexey@gmail.com>
2026-04-15 12:10:30 +0300

    Обновлена модель DTO: изменен порядок полей, для удобства

--- backend/WebApi/WebApi/Models/DTOs/Order/OrderItemsOrderDataDto.cs
+++ backend/WebApi/WebApi/Models/DTOs/Order/OrderItemsOrderDataDto.cs
@@ -5,6 +5,6 @@ public sealed class OrderItemsOrderDataDto
     public int Id { get; set; }
     public int Quantity { get; set; }
     public double Price { get; set; }
-    public double TotalPrice => Quantity * Price;
     public string NameProduct { get; set; }
+    public double TotalPrice => Quantity * Price;
 }
