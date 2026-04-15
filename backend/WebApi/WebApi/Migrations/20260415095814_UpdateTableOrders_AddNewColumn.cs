f6617bd6e0c3800ed57eb0f6729b28deea97ef25
Alexeycreator <rushev.alexey@gmail.com>
2026-04-15 14:49:41 +0300

    Добавлены новые миграции

---
+++ backend/WebApi/WebApi/Migrations/20260415095814_UpdateTableOrders_AddNewColumn.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTableOrders_AddNewColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Addresses_Id",
+                table: "Orders",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NameOrder",
+                table: "Orders",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_Addresses_Id",
+                table: "Orders",
+                column: "Addresses_Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Addresses_Addresses_Id",
+                table: "Orders",
+                column: "Addresses_Id",
+                principalTable: "Addresses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Addresses_Addresses_Id",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_Addresses_Id",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Addresses_Id",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "NameOrder",
+                table: "Orders");
+        }
+    }
+}
