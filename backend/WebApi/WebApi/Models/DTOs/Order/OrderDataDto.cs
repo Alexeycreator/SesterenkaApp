c92e89f05c1ea934e820a7ea3c5e6fcefe56bdb2
Alexeycreator <rushev.alexey@gmail.com>
2026-04-15 12:55:31 +0300

    Обновлена модель DTO: добавлено имя заказа

--- backend/WebApi/WebApi/Models/DTOs/Order/OrderDataDto.cs
+++ backend/WebApi/WebApi/Models/DTOs/Order/OrderDataDto.cs
@@ -3,6 +3,7 @@ namespace WebApi.Models.DTOs.Order;
 public sealed class OrderDataDto
 {
     public int Id { get; set; }
+    public string NameOrder { get; set; }
     public string LoginUser { get; set; }
     public string Status { get; set; }
     public DateTime OrderDate { get; set; }
