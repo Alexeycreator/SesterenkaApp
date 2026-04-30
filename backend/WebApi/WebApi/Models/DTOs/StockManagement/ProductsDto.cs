f877859f9cd378c343bae89eb934b3b8ec666019
Alexeycreator <rushev.alexey@gmail.com>
2026-04-30 21:19:17 +0300

    к предыдущему

--- backend/WebApi/WebApi/Models/DTOs/StockManagement/ProductsDto.cs
+++ backend/WebApi/WebApi/Models/DTOs/StockManagement/ProductsDto.cs
@@ -2,5 +2,7 @@ namespace WebApi.Models.DTOs.StockManagement;
 
 public class ProductsDto
 {
-    
+    public int? Id { get; set; }
+    public string? NameProduct { get; set; }
+    public string? PartNumber { get; set; }
 }
