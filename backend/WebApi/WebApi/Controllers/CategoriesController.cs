a884766f4b9d397fc31463db3a357ad234486a1a
Alexeycreator <rushev.alexey@gmail.com>
2026-05-03 11:41:15 +0300

    Реализованы методы CRUD для категорий

--- backend/WebApi/WebApi/Controllers/CategoriesController.cs
+++ backend/WebApi/WebApi/Controllers/CategoriesController.cs
@@ -31,4 +31,98 @@ public sealed class CategoriesController(ServerDbContext dbContext) : Controller
 
         return Ok(categorie);
     }
+
+    [HttpPost("create-category")]
+    public async Task<IActionResult> CreateCategories([FromBody] CategoriesModel? request)
+    {
+        try
+        {
+            if (request == null)
+            {
+                return BadRequest(new
+                    { message = "Данные не предоставлены" });
+            }
+
+            if (string.IsNullOrEmpty(request.Name) || string.IsNullOrEmpty(request.Description) ||
+                string.IsNullOrEmpty(request.Icon))
+            {
+                return BadRequest(new
+                    { message = "Название, описание и изображение категории обязательны для заполнения" });
+            }
+
+            var existingCategories = await dbContext.Categories.FirstOrDefaultAsync(c => c.Name == request.Name);
+            if (existingCategories != null)
+            {
+                return Conflict(new { message = "Категория с таким названием уже существует" });
+            }
+
+            var newCategories = new CategoriesModel()
+            {
+                Name = request.Name,
+                Icon = request.Icon,
+                Description = request.Description
+            };
+            await dbContext.Categories.AddAsync(newCategories);
+            await dbContext.SaveChangesAsync();
+
+            return Ok(new { message = "Категория успешно создана" });
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new { message = "Ошибка при обновлении", error = ex.Message });
+        }
+    }
+
+    [HttpPut("update-category")]
+    public async Task<IActionResult> UpdateCategories(int categoryId, [FromBody] CategoriesModel request)
+    {
+        try
+        {
+            var categories = await dbContext.Categories.FindAsync(categoryId);
+            if (categories == null)
+            {
+                return NotFound(new { message = "Категория не найдена" });
+            }
+
+            if (string.IsNullOrEmpty(request.Name) || string.IsNullOrEmpty(request.Description) ||
+                string.IsNullOrEmpty(request.Icon))
+            {
+                return BadRequest(new
+                    { message = "Название, описание и изображение категории обязательны для заполнения" });
+            }
+
+            categories.Name = request.Name;
+            categories.Icon = request.Icon;
+            categories.Description = request.Description;
+            await dbContext.SaveChangesAsync();
+
+            return Ok(new { message = "Категория успешно обновлена" });
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new { message = "Ошибка при обновлении", error = ex.Message });
+        }
+    }
+
+    [HttpDelete("delete-category")]
+    public async Task<IActionResult> DeleteCategories(int categoryId)
+    {
+        try
+        {
+            var categories = await dbContext.Categories.FindAsync(categoryId);
+            if (categories == null)
+            {
+                return NotFound(new { message = "Категория не найдена" });
+            }
+
+            dbContext.Categories.Remove(categories);
+            await dbContext.SaveChangesAsync();
+
+            return Ok(new { message = "Категория успешно удалена" });
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new { message = "Ошибка при обновлении", error = ex.Message });
+        }
+    }
 }
