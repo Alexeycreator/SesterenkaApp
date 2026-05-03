f34720350a7ef58c211989f8eeb70d5780507be2
Alexeycreator <rushev.alexey@gmail.com>
2026-05-03 10:56:24 +0300

    исправлена модель api для CRUD операций

--- frontend/client/src/components/servicesApi/ManufacturersApi.tsx
+++ frontend/client/src/components/servicesApi/ManufacturersApi.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-throw-literal */
 import React from "react";
 import axios from "axios";
 
@@ -17,8 +18,25 @@ export interface Manufacturer {
 };
 
 export const getManufacturers = async (): Promise<Manufacturer[]> => {
-    const response = await api.get<Manufacturer[]>('/Manufacturers');
-    return response.data;
+    try {
+        const response = await api.get<Manufacturer[]>('/Manufacturers');
+        return response.data;
+    } catch (error: any) {
+        if (error.response) {
+            console.log('Ошибка ответа:', error.response.data);
+            console.log('Статус:', error.response.status);
+
+            throw {
+                ...error,
+                serverMessage: error.response.data?.message || 'Неизвестная ошибка',
+                statusCode: error.response.status
+            };
+        } else if (error.request) {
+            throw { message: 'Нет ответа от сервера', isNetworkError: true };
+        } else {
+            throw { message: error.message, isSetupError: true };
+        }
+    }
 };
 
 export const getManufacturerById = async (id: number): Promise<Manufacturer> => {
@@ -30,17 +48,77 @@ export const getManufacturerById = async (id: number): Promise<Manufacturer> => 
             console.log('Ошибка ответа:', error.response.data);
             console.log('Статус:', error.response.status);
 
-            // eslint-disable-next-line no-throw-literal
             throw {
                 ...error,
                 serverMessage: error.response.data?.message || 'Неизвестная ошибка',
                 statusCode: error.response.status
             };
         } else if (error.request) {
-            // eslint-disable-next-line no-throw-literal
             throw { message: 'Нет ответа от сервера', isNetworkError: true };
         } else {
-            // eslint-disable-next-line no-throw-literal
+            throw { message: error.message, isSetupError: true };
+        }
+    }
+};
+
+export const createManufacturer = async (manufacturerData: { name: string }): Promise<void> => {
+    try {
+        await api.post<Manufacturer>(`/Manufacturers/create-manufacturer`, manufacturerData);
+    } catch (error: any) {
+        if (error.response) {
+            console.log('Ошибка ответа:', error.response.data);
+            console.log('Статус:', error.response.status);
+
+            throw {
+                ...error,
+                serverMessage: error.response.data?.message || 'Неизвестная ошибка',
+                statusCode: error.response.status
+            };
+        } else if (error.request) {
+            throw { message: 'Нет ответа от сервера', isNetworkError: true };
+        } else {
+            throw { message: error.message, isSetupError: true };
+        }
+    }
+};
+
+export const updateManufacturer = async (manufacturerId: number, manufacturerData: { name: string }): Promise<void> => {
+    try {
+        await api.put<Manufacturer>(`/Manufacturers/update-manufacturer?manufacturerId=${manufacturerId}`, manufacturerData);
+    } catch (error: any) {
+        if (error.response) {
+            console.log('Ошибка ответа:', error.response.data);
+            console.log('Статус:', error.response.status);
+
+            throw {
+                ...error,
+                serverMessage: error.response.data?.message || 'Неизвестная ошибка',
+                statusCode: error.response.status
+            };
+        } else if (error.request) {
+            throw { message: 'Нет ответа от сервера', isNetworkError: true };
+        } else {
+            throw { message: error.message, isSetupError: true };
+        }
+    }
+};
+
+export const deleteManufacturer = async (manufacturerId: number): Promise<void> => {
+    try {
+        await api.delete<Manufacturer>(`/Manufacturers/delete-manufacturer?manufacturerId=${manufacturerId}`);
+    } catch (error: any) {
+        if (error.response) {
+            console.log('Ошибка ответа:', error.response.data);
+            console.log('Статус:', error.response.status);
+
+            throw {
+                ...error,
+                serverMessage: error.response.data?.message || 'Неизвестная ошибка',
+                statusCode: error.response.status
+            };
+        } else if (error.request) {
+            throw { message: 'Нет ответа от сервера', isNetworkError: true };
+        } else {
             throw { message: error.message, isSetupError: true };
         }
     }
